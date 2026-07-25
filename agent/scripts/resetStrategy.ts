f1cb7e442f6cf88ffb46a448ce9f226e7d5f48b1
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-07-25 17:41:55 +0100

    add: Resetting script imports

---
+++ agent/scripts/resetStrategy.ts
@@ -0,0 +1,7 @@
+import { ethers } from "ethers";
+import * as dotenv from "dotenv";
+import * as fs from "fs";
+import * as path from "path";
+import { fileURLToPath } from "url";
+
+dotenv.config();
