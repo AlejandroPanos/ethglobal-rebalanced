03a4dadb5e6bf2a574adc2de04beb3a1edb34550
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-07-25 17:42:32 +0100

    add: __dirname & data reading

--- agent/scripts/resetStrategy.ts
+++ agent/scripts/resetStrategy.ts
@@ -5,3 +5,12 @@ import * as path from "path";
 import { fileURLToPath } from "url";
 
 dotenv.config();
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const VaultAbi = JSON.parse(
+  fs.readFileSync(path.join(__dirname, "../../abis/Vault.json"), "utf-8"),
+);
+const deployments = JSON.parse(
+  fs.readFileSync(path.join(__dirname, "../../deployments.json"), "utf-8"),
+);
